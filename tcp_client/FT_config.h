9e2ce563e11b62ceb729d64207d8e86b4d0beb2a
Phanou <cerbere74370@gmail.com>
2018-04-27 15:52:49 +0200

    Cleanup

--- tcp_client/FT_config.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-struct FT_Config			/* Assume units in SI */
-{
-	__int32 countsPerForce = 1000000;		/* Calibration Counts per force unit. */
-	__int32 countsPerTorque = 1000000;		/* Calibration Counts per torque unit. */
-};
