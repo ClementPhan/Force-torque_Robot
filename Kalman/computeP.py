0f0c27ac77b8fcfe9487824d64d58ab361b66df6
sarah <sarah_curtit@yahoo.fr>
2018-04-30 14:18:27 +0200

    correction matrices d'erreur

--- Kalman/computeP.py
+++ Kalman/computeP.py
@@ -3,11 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.linalg
 
-k = 2; 
+k = 3.1*pow(10,3); 
     
-L = 100;
+L = 30.5*pow(10,-3); #rayon bille 6.5mm
 
-m = 1;
+m = 20*pow(10,-3);
     
 g = 9.81*pow(10,-3); 
 
@@ -32,70 +32,48 @@ PMy = 0.0125
 Mz = g*L/2.
 PMz = 0.015
 
+zasp = pow(10,-3)
+zrobot = 0.2*pow(10,-3)
+
 
 def cov(x,y, Ex, Ey):
     return 
 
 
-"""A = np.array([0,0,0,1,0,0,
-              0,0,0,0,1,0,
-              0,0,0,0,0,1,
-              0,0,0,0,0,0,
-              0,0,0,0,0,0,
-              0,0,k/m,0,0,0], float).reshape(6,6);
-
-C = np.array([0,0,0,0,0,0,
-              0,0,0,0,0,0,
-              0,0,k,0,0,0,
-              0,0,0,0,0,0,
-              0,0,0,0,0,0,
-              0,0,0,0,0,0], float).reshape(6,6);
-
-W = np.array([0.0196,0,0,0,0,0,0,
-             0,0.0196,0,0,0,0,0,
-             0,0,0.0196,0,0,0,0,
-             0,0,0,0.0196,0,0,0,
-             0,0,0,0,0.0196,0,0,
-             0,0,0,0,0,0.0196,0,
-             0,0,0,0,0,0,0.09], float).reshape(7,7);
-
-M = np.array([0,0,0,1,0,0,0,
-             0,0,0,0,1,0,0,
-             0,0,0,0,0,1,0,
-             0,0,0,0,0,0,1,
-             0,0,0,0,0,0,0,
-             0,0,k/m,0,0,0,k/m], float).reshape(6,7);
-
-Q = np.dot(M,np.dot(W, np.transpose(M)))
-
-
-
-V = np.array([var(Fx,PFx), 0, 0, 0, 0, 0,
-              0, var(Fy,PFy), 0, 0, 0, 0,
-              0, 0, var(Fz,PFz), 0, 0, 0,
-              0, 0, 0, var(Mx,PMx), 0, 0,
-              0, 0, 0, 0, var(My,PMy), 0,
-              0, 0, 0, 0, 0, var(Mz,PMz)], float).reshape(6,6);"""
               
 def var(Fx, PFx):
     return pow(Fx, 2)*PFx-pow(Fx*PFx,2);
               
+              
+### matrices modèles continu
 A = np.array([0,1,
               0, 0], float).reshape(2,2);
 C = np.array([k, 0],float).reshape(1,2);
 
-V = np.array([var(Fz,PFz)], float).reshape(1,1)
+V = np.array([var(Fz,PFz)+ var(zasp,k)-var(zrobot, k)], float).reshape(1,1)
              
-W = np.array([0.0196, 0, 
-             0, 0.09]).reshape(2,2)
+W = np.array([0.0196]).reshape(1,1)
              
-M = np.array([1, 0, 
-            0, k/m], float).reshape(2,2)
+M = np.array([1,
+             0], float).reshape(2,1)
             
-Q = np.dot(M,np.dot(W, np.transpose(M))) 
 
 
-P = scipy.linalg.solve_continuous_are(np.transpose(A), np.transpose(C), Q, V);
+### matrices modèle discret
+
+Ad = np.exp(A*dt)
+
+Cd = C
+
+Vd = V/dt
+
+Wd = dt*np.dot(M,np.dot(W, np.transpose(M)))
+
+Md = np.identity(2)
+
+### Résolution de l'équation de Riccati discrète 
+
+P = scipy.linalg.solve_discrete_are(np.transpose(Ad), np.transpose(Cd), Wd, Vd);
 
 print(P)
 
